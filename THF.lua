cefc6afc0b7d04ca6d7ef430d89b283e6b033469
Miguel Nistal <nistam328@gmail.com>
2025-03-30 20:15:58 -0700

    Added thf luya

---
+++ THF.lua
@@ -0,0 +1,86 @@
+--
+-- DRK Lua by MigsDank
+-- 
+
+-- Gearswap Spec: https://docs.windower.net/addons/gearswap/reference/
+
+THF_STYLE_SET = 60
+THF_MACRO_BOOK = 6
+
+-- 
+-- Main Entry Point 
+-- 
+function get_sets() 
+	-- Sets Include
+	-- Requires sets{WeaponSkill: {}, TP: {}, Idle: {}, Precast: {}, Midcast:{}}
+    include('common.lua')
+	include('THF_sets.lua')
+	set_macro(THF_MACRO_BOOK, 1)
+	set_style(THF_STYLE_SET)
+end
+
+--
+-- Dynamic Set Funcs 
+-- 
+function equip_base_set(status) 
+    -- Weapon Sets
+    equip(sets.weapons.Current)
+
+    -- TP/Engaged Sets (Glass cannon TP vs Tanky TP)
+    if status == 'Engaged' then 
+        if player.hpp <= 50 then 
+            equip(sets.tp.FullDT)
+        else
+            equip(sets.tp.Current)
+        end
+    end
+
+    -- Idle Sets (Regen/DT)
+    if status == 'Idle' then 
+        equip(sets.idle.Default)
+    end
+end
+
+
+-- 
+-- Callback Bindings 
+-- 
+function precast(spell) 
+    -- Weaponskills
+    if spell.type == "WeaponSkill" then
+		if sets.ws[spell.english] then 
+			equip(sets.ws[spell.english])
+		else
+        	equip(sets.ws.Default)
+		end
+    end
+
+    -- Job Abilities are either specific or none
+    if spell.type == "JobAbility" then
+		if sets.ja[spell.english] then 
+			equip(sets.ja[spell.english])
+        end
+    end
+
+    -- Equip Specific Sets Last
+    if sets.precast[spell.english] then
+        equip(sets.precast[spell.english])
+    end
+end
+
+
+function midcast(spell)
+    -- Equip Specific Sets Last
+    if sets.midcast[spell.english] then
+        equip(sets.midcast[spell.english])
+    end
+end
+
+function aftercast(spell)
+    equip_base_set(player.status)
+end
+
+
+function status_change(new,old)
+    equip_base_set(new)
+end
---
+++ THF_sets.lua
@@ -0,0 +1,150 @@
+--
+-- DRK Lua by MigsDank
+-- 
+
+sets = {}
+
+--
+-- Weapon Sets
+-- 
+sets.weapons = {}
+sets.weapons.List = {
+    "Daggers Main",
+    "Savage Spam",
+}
+
+
+sets.weapons["Savage Spam"] = {
+    main="Naegling",
+    sub="Skinflayer"
+}
+
+sets.weapons["Daggers Main"] = { 
+    main="Tauret",
+    sub="Skinflayer"
+}
+
+sets.weapons.Current = sets.weapons["Daggers Main"]
+
+
+-- Idle/DT Base Set
+-- DT == 50
+-- MEva >= Eva = Regen
+-- Current DT: 51
+sets.idle = {}
+sets.idle.Default = {
+    ammo="Yamarang",
+    -- DT: -7
+    head={ name="Nyame Helm", augments={'Path: B',}},
+    -- DT: -6
+    neck={ name="Loricate Torque +1", augments={'Path: A',}},
+    -- DT: -9
+    body={ name="Nyame Mail", augments={'Path: B',}},
+    --DT: -7
+    hands={ name="Nyame Gauntlets", augments={'Path: B',}},
+    -- DT: -8
+    legs={ name="Nyame Flanchard", augments={'Path: B',}},
+    -- DT: -7
+    feet={ name="Nyame Sollerets", augments={'Path: B',}},
+    waist="Sailfi Belt +1",
+    -- DT: -7
+    left_ring="Vocane Ring",
+    right_ring="Chirich Ring",
+    left_ear="Sherida Earring",
+    right_ear="Suppanomimi",
+    back={ name="Toutatis's Cape", augments={'Accuracy+20 Attack+20','"Store TP"+10',}},
+}
+
+--
+-- TP Sets
+--
+sets.tp = {}
+
+sets.tp.List = {
+    "Default",
+    "TreasureHunter",
+    "Hybrid",
+    "FullDT",
+}
+
+-- DW > 11 for /nin 
+-- DW > 21 for /nin, > 9 for haste samba
+-- Haste = 25
+-- TA > STP = DA > 
+-- Currently 27 Haste, 10 DW
+sets.tp.Default = {
+    ammo="Yamarang",
+    head={ name="Nyame Helm", augments={'Path: B',}},
+    neck={ name="Loricate Torque +1", augments={'Path: A',}},
+    body={ name="Adhemar Jacket", augments={'DEX+10','AGI+10','Accuracy+15',}},
+    hands={ name="Nyame Gauntlets", augments={'Path: B',}},
+    legs={ name="Nyame Flanchard", augments={'Path: B',}},
+    feet={ name="Herculean Boots", augments={'Attack+24','"Triple Atk."+3','Accuracy+12',}},
+    waist="Sailfi Belt +1",
+    left_ring="Rajas Ring",
+    right_ring="Chirich Ring",
+    left_ear="Sherida Earring",
+    right_ear="Suppanomimi",
+    back={ name="Toutatis's Cape", augments={'Accuracy+20 Attack+20','"Store TP"+10',}},
+}
+sets.tp.Current = sets.tp.Default
+
+sets.tp.TreasureHunter = set_combine(sets.tp.Default, {
+    hands="Asn. Armlets +2",
+    ammo="Per. Lucky Egg",
+})
+
+sets.tp.Hybrid = set_combine(sets.tp.Default, {
+    left_ring="Vocane Ring",
+    feet={ name="Nyame Sollerets", augments={'Path: B',}},
+}) 
+
+sets.tp.FullDT = sets.idle.Default
+
+
+
+-- 
+-- Precast Sets
+--
+sets.precast = {} 
+sets.precast.Default = {
+}
+
+sets.precast.FastCast = {
+    hands={ name="Leyline Gloves", augments={'Accuracy+6','Mag. Acc.+2','"Mag.Atk.Bns."+6',}},
+    right_ear="Loquac. Earring",
+}
+
+sets.precast.Ranged = {
+}
+
+
+-- 
+-- Midcast Sets
+-- 
+sets.midcast = {}
+
+
+--
+-- WS Sets 
+-- 
+sets.ws = {}
+
+-- TP Bonus > WSD > Attack = Accuracy
+sets.ws.Default = {
+    head={ name="Nyame Helm", augments={'Path: B',}},
+    body={ name="Nyame Mail", augments={'Path: B',}},
+    hands={ name="Nyame Gauntlets", augments={'Path: B',}},
+    legs={ name="Nyame Flanchard", augments={'Path: B',}},
+    feet={ name="Nyame Sollerets", augments={'Path: B',}},
+    -- TP+250
+    right_ear={ name="Moonshade Earring", augments={'"Mag.Atk.Bns."+4','TP Bonus +250',}},
+    -- WSD: +2
+    left_ear="Ishvara Earring",
+}
+
+
+-- 
+-- Job Ability Sets
+-- 
+sets.ja = {}
