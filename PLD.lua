9e2f77a55e8c2fa6a0cd2c993254320be3688875
Miguel Nistal <nistam328@gmail.com>
2025-04-04 20:15:29 -0700

    Added pld

---
+++ PLD.lua
@@ -0,0 +1,77 @@
+--
+-- PLD Lua by MigsDank
+-- 
+
+-- Gearswap Spec: https://docs.windower.net/addons/gearswap/reference/
+
+PLD_STYLE_SET = 80
+PLD_MACRO_BOOK = 6
+
+-- 
+-- Main Entry Point 
+-- 
+function get_sets() 
+	-- Sets Include
+	-- Requires sets{WeaponSkill: {}, TP: {}, Idle: {}, Precast: {}, Midcast:{}}
+    include('common.lua')
+	include('PLD_sets.lua')
+	set_macro(PLD_MACRO_BOOK, 1)
+	set_style(PLD_STYLE_SET)
+end
+
+--
+-- Dynammic Set Funcs 
+-- 
+function equip_base_set(status) 
+    -- TP/Engaged Sets (Glass cannon TP vs Tanky TP)
+    if status == 'Engaged' then 
+        equip(sets.tp.Default)
+    end
+
+    -- Idle Sets (Regen/DT)
+    if status == 'Idle' then 
+        equip(sets.idle.Default)
+    end
+
+    -- Weapon Sets
+    equip(sets.weapons.Current)
+end
+
+
+-- 
+-- Callback Bindings 
+-- 
+function precast(spell) 
+    -- If we have a specific spell set, use it
+    if sets.precast[spell.english] then
+        equip(sets.precast[spell.english])
+    -- Default to +WSD Set and override for WS
+    elseif spell.type == "WeaponSkill" then
+        equip(sets.ws.Default)
+		if sets.ws[spell.english] then 
+			equip(sets.ws[spell.english])
+        end
+    -- Job Abilities are either specific or none
+    elseif spell.type == "JobAbility" then
+		if sets.ja[spell.english] then 
+			equip(sets.ja[spell.english])
+        end
+    -- For Magic use Fast Cast
+    elseif spell.action_type == "Magic" then 
+        equip(sets.precast.FastCast)
+    end
+end
+
+
+function midcast(spell)
+    -- ENMITY and SIRD
+end
+
+function aftercast(spell)
+    status_change(player.status)
+end
+
+
+function status_change(new,old)
+    equip_base_set(new)
+end
---
+++ PLD_sets.lua
@@ -0,0 +1,108 @@
+--
+-- DRK Lua by MigsDank
+-- 
+
+sets = {}
+
+--
+-- Weapon Sets
+-- 
+sets.weapons = {}
+sets.weapons.List = {
+    "Sakpata",
+}
+
+sets.weapons["Sakpata"] = {
+    main="Sakpata's Sword",
+    sub="Priwen",
+}
+
+
+-- Idle/DT Base Set
+sets.idle = {}
+sets.idle.Default = {
+    ammo="Staunch Tathlum",
+    head="Sakpata's Helm",
+    body={ name="Sakpata's Plate", augments={'Path: A',}},
+    hands={ name="Sakpata's Gauntlets", augments={'Path: A',}},
+    legs="Sakpata's Cuisses",
+    feet="Sakpata's Leggings",
+    neck={ name="Loricate Torque +1", augments={'Path: A',}},
+    waist="Sailfi Belt +1",
+    left_ear="Ethereal Earring",
+    right_ear="Odnowa Earring +1",
+    left_ring="Vocane Ring",
+    right_ring="Chirich Ring",
+    back={ name="Rudianos's Mantle", augments={'HP+60','Eva.+20 /Mag. Eva.+20',}},
+}
+
+--
+-- TP Sets
+--
+sets.tp = {}
+
+sets.tp.List = {
+    "Default", 
+}
+
+sets.tp.Default = sets.idle.Default
+--
+-- WS Sets 
+-- 
+sets.ws = {}
+sets.ws.Default = {
+    head={ name="Nyame Helm", augments={'Path: B',}},
+    body={ name="Nyame Mail", augments={'Path: B',}},
+    hands={ name="Nyame Gauntlets", augments={'Path: B',}},
+    legs={ name="Nyame Flanchard", augments={'Path: B',}},
+    feet={ name="Nyame Sollerets", augments={'Path: B',}},
+    -- TP+250
+    right_ear={ name="Moonshade Earring", augments={'"Mag.Atk.Bns."+4','TP Bonus +250',}},
+    -- WSD: +2
+    left_ear="Ishvara Earring",
+}
+
+-- 
+-- Job Abilities
+-- 
+sets.ja = {}
+
+
+-- 
+-- Spell Precast Sets
+--
+sets.precast = {} 
+
+-- Fast Cast
+sets.precast.FastCast = {
+    hands={ name="Leyline Gloves", augments={'Accuracy+6','Mag. Acc.+2','"Mag.Atk.Bns."+6',}},
+    legs={ name="Eschite Cuisses", augments={'"Mag.Atk.Bns."+25','"Conserve MP"+6','"Fast Cast"+5',}},
+    right_ring="Kishar Ring",
+    left_ear="Malignance Earring",
+    right_ear="Loquac. Earring",
+}
+
+
+
+-- 
+-- Spell Midcast Sets
+-- 
+sets.midcast = {}
+
+sets.midcast.SIRDEnmity = {
+    -- SIRD 10
+    ammo="Staunch Tathlum",
+    -- SIRD 15
+    head={ name="Eschite Helm", augments={'Mag. Evasion+15','Spell interruption rate down +15%','Enmity+7',}},
+    -- SIRD 5
+    neck={ name="Loricate Torque +1", augments={'Path: A',}},
+    -- SIRD 15
+    body={ name="Eschite Breast.", augments={'Mag. Evasion+15','Spell interruption rate down +15%','Enmity+7',}},
+}
+
+
+
+sets.midcast.Default = {
+}
+
+
