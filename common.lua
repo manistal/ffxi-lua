f133ce688cc87042e991ee3a42bfcdb2b09412ce
Miguel Nistal <nistam328@gmail.com>
2025-05-31 22:07:17 -0700

    Cleaning up toggles

--- common.lua
+++ common.lua
@@ -73,42 +73,6 @@ function bind_toggles(key, set_category)
     toggle_binds[set_category].callbackfunc()
 end
 
---
--- Toggle Weapons
--- 
-local weapon_toggle_idx = 0
-function toggle_weapons()
-    weapon_toggle_idx = ((weapon_toggle_idx + 1) % (#sets.weapons.List + 1))
-    if weapon_toggle_idx == 0 then weapon_toggle_idx = 1 end 
-    sets.weapons.Current = sets.weapons[sets.weapons.List[weapon_toggle_idx]]
-    equip(sets.weapons.Current)
-	send_command("@input /echo <<<< Weapon Set changed to " .. sets.weapons.List[weapon_toggle_idx] .. " " .. weapon_toggle_idx .. " >>>>")
-end
-
--- 
--- Toggle Engaged/TP 
--- 
-local tp_toggle_idx = 0
-function toggle_tp()
-    tp_toggle_idx = ((tp_toggle_idx + 1) % (#sets.tp.List + 1))
-    if tp_toggle_idx == 0 then tp_toggle_idx = 1 end 
-    sets.tp.Current = sets.tp[sets.tp.List[tp_toggle_idx]]
-    equip(sets.tp.Current)
-	send_command("@input /echo <<<< TP Set changed to " .. sets.tp.List[tp_toggle_idx] .. " " .. tp_toggle_idx .. " >>>>")
-end
-
--- 
--- Toggle Idle
--- 
-local idle_toggle_idx = 0
-function toggle_idle()
-    idle_toggle_idx = ((idle_toggle_idx + 1) % (#sets.idle.List + 1))
-    if idle_toggle_idx == 0 then idle_toggle_idx = 1 end 
-    sets.idle.Current = sets.idle[sets.idle.List[idle_toggle_idx]]
-    equip(sets.idle.Current)
-	send_command("@input /echo <<<< Idle Set changed to " .. sets.idle.List[idle_toggle_idx] .. " " .. idle_toggle_idx .. " >>>>")
-end
-
 -- Cities and Zones
 windower.register_event('zone change', 
 	function()
@@ -129,13 +93,13 @@ function self_command(command)
     if #cmdArgs < 0 then 
         return 
     end
-
     local cmd = cmdArgs[0]
+
     if ((cmd == "test") and (#cmdArgs >= 2)) then 
         test_equip(cmdArgs)
     end
+
     if (cmd == "toggle") then 
-        windower.add_to_chat("Toggling category: " .. command .. " " .. cmdArgs[1])
         if (#cmdArgs < 1) then 
             windower.add_to_chat("Usage: gs c toggle <category>"..#cmdArgs)
             return
@@ -146,14 +110,5 @@ function self_command(command)
         else
             windower.add_to_chat("No toggle bind for category: " .. set_category)
         end
-    end
-    if (cmd == "weapons") then
-        toggle_weapons()
-    end
-    if (cmd == "tp") then
-        toggle_tp()
-    end
-    if (cmd == "idle") then
-        toggle_idle()
     end
 end
